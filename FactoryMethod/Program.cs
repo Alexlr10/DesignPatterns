597d60a23954f2a98458dbb24e77631536679da2
Alexlr10 <alexlr10@hotmail.com>
2021-02-02 15:53:49 -0300

    Inserindo FactoryMethod

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FactoryMethod fm = new FactoryMethod();
+
+            Console.WriteLine("Liu Kang | Scorpion | SubZero");
+            Console.WriteLine();
+
+            Console.WriteLine("Escolha seu personagem");
+            string escolha = Console.ReadLine();
+
+            IPersonagem personagem = fm.EscolherPersonagem(escolha);
+            Console.WriteLine();
+            Console.WriteLine("Voce vai jogar com ");
+            personagem.Escolhido();
+
+            Console.ReadKey();
+        }
+    }
+}
