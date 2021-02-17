6b49f246281638374aa355d761126895a94682e8
Alexlr10 <alexlr10@hotmail.com>
2021-02-17 14:19:18 -0300

    Inserindo Facade

---
+++ Facade/SubSistemaTres.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Facade
+{
+    public class SubSistemaTres
+    {
+        public void MetodoTres()
+        {
+            Console.WriteLine("Metodo do SubSistema Tres");
+        }
+    }
+}
